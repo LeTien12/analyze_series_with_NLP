5d37b470807a8a0e12ebeff52c1d4c7539393f07
TienLe12 <tle38413@gmail.com>
2024-08-28 11:08:41 +0700

    load file

--- functions/functions.py
+++ functions/functions.py
@@ -5,12 +5,13 @@ from theme_classifier.themes_classifier import ThemeClassifier
 def get_themes(theme_list , subtitles_path , save_path):
     theme_list = theme_list.split(',')
     theme_classifier = ThemeClassifier(theme_list)
-    output_df = theme_classifier.get_themes(subtitles_path , save_path)
+    output_theme = theme_classifier.get_themes(subtitles_path , save_path)
     
     #Remove dialogue from the theme list
     
     theme_list = [theme for theme in theme_list if theme != 'dialogue']
-    output_df = output_df[theme_list].sum().reset_index()
+    output_df = output_theme[theme_list]
+    output_df = output_df.sum().reset_index()
     output_df.columns = ['theme' , 'score']
     
     output_chart = gr.BarPlot(
